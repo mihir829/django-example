9312b19fbd32a425f312ae0cfff8aa5a5ef51c21
unknown <190123116013@git.org.in>
2021-06-24 16:43:57 +0530

    first

---
+++ learning_user/basic_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo
+# Register your models here.
+admin.site.register(UserProfileInfo)
+
